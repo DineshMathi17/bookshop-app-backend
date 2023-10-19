646f0251000fbca8efe391c61bccb0d896970773
Dinesh <121874583+DineshMathi17@users.noreply.github.com>
2023-10-19 18:11:06 +0530

    Update stripe.js

--- routes/stripe.js
+++ routes/stripe.js
@@ -7,28 +7,6 @@ const stripe = Stripe(process.env.STRIPE_KEY);
 
 const router = express.Router();
 
-// router.post("/create-checkout-session", async (req, res) => {
-//   const session = await stripe.checkout.sessions.create({
-//     line_items:[
-//       {
-//         price_data:{
-//           currency: "INR",
-//           product_data: {
-//             name: "book",
-//           },
-//           unit_amount: 500,
-//         },
-//         quantity:1,
-//       }
-//     ],
-//     mode: "payment",
-//     success_url: `${process.env.CLIENT_URL}/checkout-success`,
-//     cancel_url: `${process.env.CLIENT_URL}/cart`,
-//   });
-
-//   // res.redirect(303, session.url);
-//   res.send({ url: session.url });
-// });
 
 router.post("/create-checkout-session", async (req, res) => {
   const customer = await stripe.customers.create({
@@ -58,53 +36,7 @@ router.post("/create-checkout-session", async (req, res) => {
 
   const session = await stripe.checkout.sessions.create({
      payment_method_types: ["card"],
-    // shipping_address_collection: {
-    //   allowed_countries: ["US", "CA", "KE"],
-    // },
-    // shipping_options: [
-    //   {
-    //     shipping_rate_data: {
-    //       type: "fixed_amount",
-    //       fixed_amount: {
-    //         amount: 0,
-    //         currency: "usd",
-    //       },
-    //       // display_name: "Free shipping",
-    //       // Delivers between 5-7 business days
-    //       delivery_estimate: {
-    //         minimum: {
-    //           unit: "business_day",
-    //           value: 5,
-    //         },
-    //         maximum: {
-    //           unit: "business_day",
-    //           value: 7,
-    //         },
-    //       },
-    //     },
-    //   },
-    //   {
-    //     shipping_rate_data: {
-    //       type: "fixed_amount",
-    //       fixed_amount: {
-    //         amount: 1500,
-    //         currency: "usd",
-    //       },
-    //       display_name: "Next day air",
-    //       // Delivers in exactly 1 business day
-    //       delivery_estimate: {
-    //         minimum: {
-    //           unit: "business_day",
-    //           value: 1,
-    //         },
-    //         maximum: {
-    //           unit: "business_day",
-    //           value: 1,
-    //         },
-    //       },
-    //     },
-    //   },
-    // ],
+    
     phone_number_collection: {
       enabled: true,
     },
